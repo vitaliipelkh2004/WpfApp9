405fe635131a09f1b410814236cedf66eb9f1507
vitaliipelkh2004 <995nik@gmail.com>
2019-06-10 14:51:23 +0300

    Tetxtbox

---
+++ WpfApp9/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp9
+{
+    public class Storage
+    {
+        public string Name {set;get;}
+        public int Days { set; get; }
+        public int Width { set; get; }
+        public int Height { set; get; }
+
+
+
+
+
+    }
+}
