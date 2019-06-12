4cd29067e45ab0e59ae664e4fd3b04c38482e52c
vitaliipelkh2004 <995nik@gmail.com>
2019-06-12 13:12:51 +0300

    small update

--- WpfApp9/MainWindow.xaml.cs
+++ WpfApp9/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace WpfApp9
         List<Storage> items = new List<Storage>();
         DateTime now = new DateTime(2019, 6, 11);
         Storage item;
+        int Area=10000;
         ObservableCollection<Storage> itemsnew = new ObservableCollection<Storage>();
         private int current = -1;
         public MainWindow()
@@ -33,28 +34,48 @@ namespace WpfApp9
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            item = new Storage();
-            item.Owner = ownertext.Text;
-            item.Name = nametext.Text;
-            item.Height = Int32.Parse(heighttext.Text);
-            item.Width = Int32.Parse(widhttext.Text);
-            item.Days = Int32.Parse(daystext.Text);
-            item.Money = item.Days * 5;
-            item.Arrears = 0;
-            datepicker1.IsEnabled = true;
-            items.Add(item);
-            var itemss = main.Children;
-            foreach (var item in itemss)
+            try
             {
-                if (item is TextBox textbox1)
+                var itemss = main.Children;               
+                item = new Storage();
+                item.Owner = ownertext.Text;
+                item.Name = nametext.Text;
+                item.Height = Int32.Parse(heighttext.Text);
+                item.Width = Int32.Parse(widhttext.Text);
+                item.Days = Int32.Parse(daystext.Text);
+                item.Money = item.Days * 5;
+                item.Arrears = 0;
+                datepicker1.IsEnabled = true;
+                items.Add(item);
+                Area = Area - item.Width * item.Height;
+                if (Area <= 0)
                 {
-                    textbox1.Clear();
+                    MessageBox.Show("Storage is full");
+                    foreach (var item in itemss)
+                    {
+                        if (item is TextBox textbox1)
+                        {
+                            textbox1.Clear();
+                        }
+                    }
+                    return;
                 }
-            }
+                foreach (var item in itemss)
+                {
+                    if (item is TextBox textbox1)
+                    {
+                        textbox1.Clear();
+                    }
+                }
             XmlSerializer xml = new XmlSerializer(typeof(List<Storage>));
-            using (FileStream t = new FileStream("GAME1.xml", FileMode.OpenOrCreate, FileAccess.Write))
+            using (FileStream t = new FileStream("1.xml", FileMode.OpenOrCreate, FileAccess.Write))
             {
                 xml.Serialize(t,items);
+
+            }
+            }
+            catch
+            {
 
             }
         }
@@ -77,44 +98,75 @@ namespace WpfApp9
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            itembox.Visibility = Visibility;
             XmlSerializer xml = new XmlSerializer(typeof(ObservableCollection<Storage>));
-            FileStream t = new FileStream("GAME1.xml", FileMode.Open, FileAccess.Read);
+            FileStream t = new FileStream("1.xml", FileMode.Open, FileAccess.Read);
             itemsnew = (ObservableCollection<Storage>)xml.Deserialize(t);
             t.Close();
             this.DataContext = itemsnew[1];
+            deletebutton.IsEnabled = true;
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            if (current == -1)
+            try
             {
-                this.DataContext = itemsnew.FirstOrDefault();
-                current = 0;
+                if (current == -1)
+                {
+                    this.DataContext = itemsnew.FirstOrDefault();
+                    current = 0;
 
-            }
-            else if (current == 0)
-            {
-                this.DataContext = itemsnew.LastOrDefault();
-                current = itemsnew.Count - 1;
+                }
+                else if (current == 0)
+                {
+                    this.DataContext = itemsnew.LastOrDefault();
+                    current = itemsnew.Count - 1;
 
+                }
+                else
+                {
+                    this.DataContext = itemsnew[current-- - 1];
+                }
             }
-            else
+            catch 
             {
-                this.DataContext = itemsnew[current-- - 1];
+
             }
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            if (current == -1 || current == itemsnew.Count - 1)
+            try
             {
-                this.DataContext = itemsnew.FirstOrDefault();
-                current = 0;
+                if (current == -1 || current == itemsnew.Count - 1)
+                {
+                    this.DataContext = itemsnew.FirstOrDefault();
+                    current = 0;
+                }
+                else
+                {
+                    this.DataContext = itemsnew[++current];
+                }
             }
-            else
+            catch
             {
-                this.DataContext = itemsnew[++current];
+
             }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"To pay {itemsnew[current].Arrears+itemsnew[current].Money}");
+
+            itemsnew.RemoveAt(current);
+            try
+            {
+                this.DataContext = itemsnew.LastOrDefault();
+
+            }
+            catch
+            { }
+
         }
     }  
 }
